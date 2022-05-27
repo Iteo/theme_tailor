6f46bb6c8a67548086903f31418261b51fe05f4a
Paweł Franitza <pawel.franitza@iteo.com>
2022-05-28 01:32:05 +0200

    Add Melos

--- packages/theme_tailor_annotation/lib/theme_tailor_annotation.dart
+++ packages/theme_tailor_annotation/lib/theme_tailor_annotation.dart
@@ -1,9 +1,8 @@
 library theme_tailor_annotation;
 
-export 'src/model/typedefs.dart';
-export 'src/model/theme_encoder.dart';
 export 'src/model/text_data.dart';
-
-export 'src/theme/theme_tailor.dart';
+export 'src/model/theme_encoder.dart';
+export 'src/model/typedefs.dart';
 export 'src/theme/tailor_prop.dart';
+export 'src/theme/theme_tailor.dart';
 export 'src/theme/theme_tailor_environment.dart';
