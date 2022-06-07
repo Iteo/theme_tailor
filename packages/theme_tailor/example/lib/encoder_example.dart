c750e0c69188f120741c21c887cfcc978c0182e8
Paweł Franitza <pawel.franitza@iteo.com>
2022-06-07 12:48:15 +0200

    Examples cleanup + docs

---
+++ packages/theme_tailor/example/lib/encoder_example.dart
@@ -0,0 +1,95 @@
+import 'package:example/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:theme_tailor_annotation/theme_tailor_annotation.dart';
+
+part 'encoder_example.tailor.dart';
+
+class TextStyleEncoder extends ThemeEncoder<TextStyle> {
+  const TextStyleEncoder();
+
+  @override
+  TextStyle lerp(TextStyle a, TextStyle b, double t) {
+    return TextStyle.lerp(a, b, t)!;
+  }
+}
+
+class CustomColorEncoder extends ThemeEncoder<Color> {
+  const CustomColorEncoder();
+
+  @override
+  Color lerp(Color a, Color b, double t) {
+    return Color.fromARGB(
+      _lerpColorComponent(a.alpha, b.alpha, t),
+      _lerpColorComponent(a.red, b.red, t),
+      _lerpColorComponent(a.green, b.green, t),
+      _lerpColorComponent(a.blue, b.blue, t),
+    );
+  }
+
+  int _lerpColorComponent(int a, int b, double t) {
+    return _clampInt(_lerpInt(a, b, t), 0, 255);
+  }
+
+  double _lerpInt(int a, int b, double t) => a + (b - a) * t;
+
+  int _clampInt(double value, int min, int max) {
+    return value < min
+        ? min
+        : value > max
+            ? max
+            : value.toInt();
+  }
+}
+
+/// Use @Tailor annotation to declare custom themes or different
+/// quantity of themes than the default 2 (light and dark)
+@Tailor(themes: ['superLight', 'amoledDark'])
+class $_Theme1 {
+  @CustomColorEncoder()
+  static List<Color> background = [AppColors.white, AppColors.black];
+  static List<Color> iconColor = [AppColors.orange, AppColors.blue];
+
+  @TextStyleEncoder()
+  static List<TextStyle> h1 = const [TextStyle(), TextStyle()];
+  static List<TextStyle> h2 = const [TextStyle(), TextStyle()];
+}
+
+const customColorEncoder = CustomColorEncoder();
+const textStyleEncoder = TextStyleEncoder();
+
+@Tailor(themes: ['superLight', 'amoledDark'])
+class $_Theme2 {
+  @customColorEncoder
+  static List<Color> background = [AppColors.white, AppColors.black];
+  static List<Color> iconColor = [AppColors.orange, AppColors.blue];
+
+  @textStyleEncoder
+  static List<TextStyle> h1 = const [TextStyle(), TextStyle()];
+  static List<TextStyle> h2 = const [TextStyle(), TextStyle()];
+}
+
+@Tailor(
+  themes: ['superLight', 'amoledDark'],
+  encoders: [CustomColorEncoder(), TextStyleEncoder()],
+)
+class $_Theme3 {
+  static List<Color> background = [AppColors.white, AppColors.black];
+  static List<Color> iconColor = [AppColors.orange, AppColors.blue];
+
+  static List<TextStyle> h1 = const [TextStyle(), TextStyle()];
+  static List<TextStyle> h2 = const [TextStyle(), TextStyle()];
+}
+
+const tailorWithEncoders = Tailor(
+  themes: ['superLight', 'amoledDark'],
+  encoders: [customColorEncoder, textStyleEncoder],
+);
+
+@tailorWithEncoders
+class $_Theme4 {
+  static List<Color> background = [AppColors.white, AppColors.black];
+  static List<Color> iconColor = [AppColors.orange, AppColors.blue];
+
+  static List<TextStyle> h1 = const [TextStyle(), TextStyle()];
+  static List<TextStyle> h2 = const [TextStyle(), TextStyle()];
+}
--- packages/theme_tailor_annotation/lib/src/theme_encoder.dart
+++ packages/theme_tailor_annotation/lib/src/theme_encoder.dart
@@ -1,8 +1,21 @@
+/// {@template theme_tailor.theme_encoder}
+/// ### ThemeEncoder
 /// Implement this class to provide custom encoding for a specific [Type]
 /// It allows for specyfying custom logic for encoding the theme values.
 /// [T] is the type of the encoded data such as "Color" or "TextStyle"
-
+///
 /// [ThemeEncoder]s can be placed either on a class or a field
+///
+/// If field is annotated with [ThemeEncoder] this encoder will be used,
+/// otherwise encoders from the @Tailor(encoders: [...])
+/// or annotations below @Tailor() annotation will be used.
+/// If no matching encoder for the type is found,
+/// default one (dynamic) will be used.
+///
+/// Default encoders are implemented for:
+/// * TextStyle, TextStyle?
+/// * Color, Color?
+/// * dynamic
 ///
 /// ```dart
 /// class CustomColorEncoder extends ThemeEncoder<Color> {
@@ -17,18 +30,23 @@
 /// }
 ///
 /// @Tailor()
-/// @CustomColorEncoder()
 /// class AppTheme {
 ///   static const List<Color> surface = [Colors.white, Colors.black];
 ///
 ///   @CustomColorEncoder()
 ///   static const List<Color> background = [Colors.blue, Colors.purple];
 ///
+///   /// This will use the default encoder for Color
+///   static const List<Color> surface = [Colors.blue, Colors.purple];
+///
+///   /// This will the dynamic encoder since the type of the list is dynamic
+///   static const List someColors = [Colors.blue, Colors.purple];
+///
 ///   @CustomTextStyleEncoder()
 ///   static const List<TextStyle> h1 = [TextStyle(), TextStyle()];
 /// }
 /// ```
-///
+/// {@endtemplate}
 abstract class ThemeEncoder<T extends Object> {
   const ThemeEncoder();
 
@@ -38,28 +56,4 @@ abstract class ThemeEncoder<T extends Object> {
   /// 0.0: interpolation has not started, returns a;
   /// 1.0: interpolation has finished, returns b;
   T lerp(T a, T b, double t);
-}
-
-/// Implementation of [ThemeEncoder] for any object
-/// Value is not interpolated between 2 states and is either A or B
-/// Extend SimpleEncoder with a given type to bind it to the theme data type.
-/// e.g.:
-/// ```dart
-/// class ColorSimpleEncoder extends SimpleEncoder<Color> {
-///   const ColorSimpleEncoder();
-/// }
-///
-/// const colorEncoder = ColorSimpleEncoder();
-/// ```
-/// Then use the annotation on the class annotated with @tailor
-/// ```dart
-/// @tailor
-/// @colorEncoder
-/// class _$SimpleTheme {}
-/// ```
-abstract class SimpleThemeEncoder<T extends Object> extends ThemeEncoder<T> {
-  const SimpleThemeEncoder();
-
-  @override
-  T lerp(T a, T b, double t) => t < .5 ? a : b;
 }
