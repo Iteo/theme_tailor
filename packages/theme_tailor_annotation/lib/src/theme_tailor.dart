70c4cac40914e568af9ed3a1d8f428134e1d7684
Paweł Franitza <pawel.franitza@iteo.com>
2022-06-08 16:03:46 +0200

    Add ThemeGetter to annotation

--- packages/theme_tailor_annotation/lib/src/theme_tailor.dart
+++ packages/theme_tailor_annotation/lib/src/theme_tailor.dart
@@ -1,15 +1,25 @@
 import 'package:meta/meta_meta.dart';
 
+import 'theme_getter.dart';
+
 const tailor = Tailor();
 
 /// {@template theme_tailor.theme_tailor}
 /// ### Tailor
 ///
 /// {@endtemplate}
-///
 @Target({TargetKind.classType})
 class Tailor {
-  const Tailor({this.themes = const ['light', 'dark']});
+  /// {@macro theme_tailor.theme_tailor}
+  const Tailor({
+    this.themes = const ['light', 'dark'],
+    this.themeGetter = ThemeGetter.onBuildContextProps,
+  });
 
   final List<String> themes;
+
+  /// Create getters for the easy access of the theme properties
+  /// In case of creating component/modular themes, set it to
+  /// [ThemeGetter.none]
+  final ThemeGetter themeGetter;
 }
--- packages/theme_tailor_annotation/lib/theme_tailor_annotation.dart
+++ packages/theme_tailor_annotation/lib/theme_tailor_annotation.dart
@@ -1,5 +1,6 @@
 library theme_tailor_annotation;
 
 export 'src/theme_encoder.dart';
+export 'src/theme_getter.dart';
 export 'src/theme_tailor.dart';
 export 'src/typedefs.dart';
