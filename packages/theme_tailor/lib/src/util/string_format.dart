0196b6536a861ea7febd2d7afd6d4265b39569aa
Paweł Franitza <pawel.franitza@iteo.com>
2022-06-09 23:57:53 +0200

    Fix StringFormat

--- packages/theme_tailor/lib/src/util/string_format.dart
+++ packages/theme_tailor/lib/src/util/string_format.dart
@@ -4,19 +4,19 @@ class StringFormat {
   static const String dynamic = 'dynamic';
   static const String nullSuffix = '?';
 
-  String themeClassName(String val, [String suffix = 'Theme']) {
-    final name = val.replaceFirst(r'_$', '').replaceFirst(r'$_', '');
-    if (name.isNotEmpty) val.firstToUpper();
+  String themeClassName(String val, [String suffix = 'CustomTheme']) {
+    if (val.isEmpty) return suffix;
+
+    final shouldTrimFirst = val[0] == '_' || val[0] == r'$';
+    if (val.length == 1 && shouldTrimFirst) return suffix;
+
+    final name = (shouldTrimFirst ? val.trimFirst() : val).firstToUpperUnsafe();
     return name != val ? name : '$name$suffix';
   }
 
-  String asCammelCase(String val) {
-    return val.isEmpty ? val : val.firstToLower();
-  }
+  String asCammelCase(String val) => val.firstToLower();
 
-  String asPrivate(String val) {
-    return val.isEmpty ? val : '_$val';
-  }
+  String asPrivate(String val) => '_$val';
 
   String asNullableType(String val) {
     if (val == dynamic) return dynamic;
@@ -26,12 +26,27 @@ class StringFormat {
 
   String typeAsVariableName(String val, [String suffix = '_']) {
     final name = asCammelCase(val);
-    print(name);
     return val != name ? name : '$name$suffix';
   }
 }
 
 extension StringExtension on String {
-  String firstToUpper() => '${this[0].toUpperCase()}${substring(1)}';
-  String firstToLower() => '${this[0].toLowerCase()}${substring(1)}';
+  String trimFirst() => isEmpty ? this : substring(1);
+
+  String firstToUpper() => _safeSubstring(toUpper: true);
+  String firstToLower() => _safeSubstring(toUpper: false);
+
+  String firstToUpperUnsafe() => this[0].toUpperCase() + substring(1);
+  String firstToLowerUnsafe() => this[0].toLowerCase() + substring(1);
+
+  String get firstAsUpper => this[0].toUpperCase();
+  String get firstAsLower => this[0].toLowerCase();
+
+  bool _canRunSubstring(String val) => val.length > 1;
+  String _safeSubstring({required bool toUpper}) {
+    late final first = toUpper ? firstAsUpper : firstAsLower;
+    if (isEmpty) return this;
+    if (!_canRunSubstring(this)) return first;
+    return first + substring(1);
+  }
 }
