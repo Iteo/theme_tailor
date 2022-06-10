5bccd16621a3d4bf53df19a4585b43e7cd7864fd
Paweł Franitza <pawel.franitza@iteo.com>
2022-06-10 13:49:27 +0200

    Change to absolute imports

--- packages/theme_tailor/lib/src/util/theme_getter_helper.dart
+++ packages/theme_tailor/lib/src/util/theme_getter_helper.dart
@@ -1,6 +1,5 @@
 import 'package:source_gen/source_gen.dart';
-
-import '../model/theme_getter_data.dart';
+import 'package:theme_tailor/src/model/theme_getter_data.dart';
 
 ExtensionData themeGetterDataFromData(ConstantReader dartObject) {
   final value = dartObject.revive().accessor.split('.').last;
