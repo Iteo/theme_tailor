14677dca043dc354758cf9f2318cd5744cc99f01
Rongix <pawel.franitza@iteo.com>
2023-03-14 09:55:56 +0100

    Docs theme_tailor: Update examples, update readme (#61)
    Update readme, update examples

--- packages/theme_tailor/example/lib/constant_theme.dart
+++ packages/theme_tailor/example/lib/constant_theme.dart
@@ -4,14 +4,18 @@ import 'package:example/diagnosticable_lib.dart';
 
 part 'constant_theme.tailor.dart';
 
-class SomeClass {
-  const SomeClass({
-    required this.x,
-    required this.y,
-  });
+class Point {
+  const Point({required this.x, required this.y});
 
   final String x;
   final String y;
+}
+
+class TwoColors {
+  const TwoColors(this.first, this.second);
+
+  final Color first;
+  final Color second;
 }
 
 class ClassWithList<T> {
@@ -25,20 +29,31 @@ class _$ConstantTheme {
   static const someNumberList = [1, 2];
 
   static const someClass = [
-    SomeClass(x: '', y: ''),
-    SomeClass(x: '1:2;3', y: '[1,]2,3'),
+    Point(x: '', y: ''),
+    Point(x: '1:2;3', y: '[1,]2,3'),
   ];
 
   static const appBarColor = [Colors.green, Colors.grey];
 
-  static const doubleList = [
+  /// THIS WONT WORK:
+  /// For passing several color / textStyle properties use another ThemeExtension
+  /// or a class that takes several color properties
+  static const notWorkingColorList = [
     [Colors.green, Colors.grey],
     [Colors.white, Colors.pink],
   ];
 
+  /// THIS WILL WORK BUT:
+  /// Preferably for working with Colors and TextThemes, use
+  /// ThemeExtension class as its properties will be animated during theme change
+  static const workingColorList = [
+    TwoColors(Colors.green, Colors.grey),
+    TwoColors(Colors.white, Colors.pink),
+  ];
+
   static const nestedLists = [
     [
-      ClassWithList(list: [1, 2, 3])
+      ClassWithList(list: ['1', '2', '3'])
     ],
     [
       ClassWithList(
